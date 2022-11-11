1862166682df01f0d73a344965f0cf22a87114ce
vandu1979 <vandana_kothari@yahoo.com>
2022-11-10 22:22:30 -0700

    Final project

--- assignment/game.js
+++ assignment/game.js
@@ -1,7 +1,7 @@
 
 class SpaceShip {
     constructor(name, hull, firepower, accuracy, isAlive) {
-    this.name = "EARTHSHIP"
+    this.name = "Millenium Falcon"
     this.hull =  20;
     this.firepower = 5;
     this.accuracy = .7;
@@ -17,7 +17,7 @@ class AlienShip {
     constructor(name, hull, firepower, accuracy, isAlive) {
     //Math.random() * (max - min) + min --  //it says to use this for a range(max is less than (and not equal)max)
     // A floating-point, pseudo-random number between 0 (inclusive) and 1 (exclusive).
-   this.name = name;
+    this.name = name;
     this.hull = Math.round((Math.random() * (6 - 3) + 3));
     this.firepower = Math.round((Math.random() * (4 - 2) + 2));
     this.accuracy = (Math.random() * (.8 - .6) + .6);
@@ -43,18 +43,18 @@ function attack(AttackerShip,TargetShip)
     let TargetHull = TargetShip.hull - AttackerShip.firepower
     if(TargetHull <= 0) 
     {
-      console.log(TargetShip.name, ' is destroyed');
+      console.log(TargetShip.name, 'is destroyed');
       TargetShip.hull=0
       TargetShip.isAlive=false
     } 
     else 
     { 
-      TargetShip.hull=TargetShip.hull - AttackerShip.firepower
-      console.log(AttackerShip.name,' Successfully Hit', TargetShip.name, ",still alive with hull", TargetShip.hull );
+      TargetShip.hull = TargetShip.hull - AttackerShip.firepower
+      console.log(AttackerShip.name,' successfully hit', TargetShip.name, ", still alive with hull", TargetShip.hull );
     } 
   }
   else {
-    console.log(AttackerShip.name,' Could not Hit', TargetShip.name );
+    console.log(AttackerShip.name,'could not hit', TargetShip.name );
   }
 }
  
@@ -74,16 +74,17 @@ function game()
     while (alienArray[i].isAlive && EarthShip.isAlive)
     {
       //Earthship to attack thr alianship
-      attack(EarthShip,alienArray[i])
+      attack(EarthShip, alienArray[i])
       if (alienArray[i].isAlive)
       {
         //if Alianship is alive, alianship attacks
-        attack(alienArray[i],EarthShip)
+        attack(alienArray[i], EarthShip)
       }
     }
     if (!EarthShip.isAlive)
     {
-      console.log("the EarthShip is dead...... End of Game")
+      console.log("The", EarthShip.name, "is dead......") 
+      console.log("End of Game")
       break;
     }
 
@@ -92,7 +93,7 @@ function game()
 }
 
 //call the function
-const button = document.querySelector("#start");
+const button = document.querySelector("#button");
 // add an event listener to the button
 button.addEventListener("click", game)
 
